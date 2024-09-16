c0f20240f65820f43a4a168749824f38c07679e9
Arroneous2 <thumpinbassbeat@gmail.com>
2024-09-16 11:17:42 -0500

    add signup and login

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,8 +1,12 @@
 import { BookshelfIndex } from "./BookSearch";
+import { Signup } from "./Signup";
+import { Login } from "./Login";
 
 export function Content() {
   return (
     <main>
+      <Signup />
+      <Login />
       <BookshelfIndex />
     </main>
   );
