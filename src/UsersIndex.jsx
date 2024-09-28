abd973bf032e2ccfdac2b17aab81e3500f4289aa
Arroneous2 <thumpinbassbeat@gmail.com>
2024-09-28 12:40:41 -0500

    make users clickable

--- src/UsersIndex.jsx
+++ src/UsersIndex.jsx
@@ -23,10 +23,21 @@ export function UsersIndex() {
     <div>
       <h1>All Users</h1>
       {users.map((user) => (
-        <div key={user.id}>
-          <h2> {user.first_name}</h2>
-          <h2> {user.last_name}</h2>
-          <h3> {user.email}</h3>
+        <div
+          key={user.id}
+          onClick={() => alert(`Clicked on ${user.first_name} ${user.last_name}`)}
+          role="button"
+          tabIndex={0}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === " ") {
+              e.preventDefault();
+              alert(`Clicked on ${user.first_name} ${user.last_name} via keyboard!`);
+            }
+          }}
+        >
+          <h2>{user.first_name}</h2>
+          <h2>{user.last_name}</h2>
+          <h3>{user.email}</h3>
         </div>
       ))}
     </div>
