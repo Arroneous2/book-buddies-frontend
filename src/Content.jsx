580516082701673bf4412ba8540ad1fc9e96b34f
Arroneous2 <thumpinbassbeat@gmail.com>
2024-09-28 12:22:26 -0500

    start to make areas linked pages

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,14 +2,17 @@ import { BookSearch } from "./BookSearch";
 import { Signup } from "./Signup";
 import { Login } from "./Login";
 import { UsersIndex } from "./UsersIndex";
+import { Routes, Route } from "react-router-dom";
 
 export function Content() {
   return (
     <main>
-      <Signup />
-      <Login />
-      <BookSearch />
-      <UsersIndex />
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/book-search" element={<BookSearch />} />
+        <Route path="/users-index" element={<UsersIndex />} />
+      </Routes>
     </main>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <header>
       <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+        <ul>
+          <li>
+            <Link to="/#">Home</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/book-search">Book Search</Link>
+          </li>
+          <li>
+            <Link to="/users-index">Users Index</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
